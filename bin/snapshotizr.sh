3845ccd08885a94e4ca34d83cc535110a60570fd
Wojciech Krawczyk <wojtekk@gmail.com>
2016-06-22 23:45:52 +0200

    Pass arguments to cli.js

--- bin/snapshotizr.sh
+++ bin/snapshotizr.sh
@@ -8,4 +8,4 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-node "$DIR/../src/cli.js"
+node "$DIR/../src/cli.js" "$@"
